ada0f18dd3cee10e7518f6796243df048f8bf248
Debarshi Dutta <ddutta@nvidia.com>
2022-08-18 17:34:48 +0530

    rust: add outer_loop label

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,8 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-}
 use std::str::FromStr;
 use std::env;
+
+fn array_loop() {
+    let v = vec![4, 7, 8, 9, 11, 10];
+
+    'outer_loop: for i in 0..10 {
+        for n in &v {
+            if i + n == 11 {
+                break 'outer_loop;
+            }
+            println!("Hello Vector {} {}",i, n);
+        }
+    }
+}
+
+fn loop_to_10() {
+    for n in 0..10 {
+        println!("Hello {}", n);
+    }
+}
 
 fn main() {
     let mut numbers = Vec::new();
@@ -22,6 +38,9 @@ fn main() {
     primes.push(13);
 
     numbers.sort();
+
+    array_loop();
+    loop_to_10();
 
     println!("The sorted numbers are {:?}", numbers);
     println!("The prime numbers are {:?}", primes);
